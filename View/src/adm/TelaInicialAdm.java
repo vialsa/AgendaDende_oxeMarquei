21e05d1da85090115f87c31b60557b00fcb8293a
Vitor Almeida Santos <vitorsjtbarbosa@outlook.com>
2024-04-22 14:11:26 -0300

    Telas ADM RESPONSIVAS

--- View/src/adm/TelaInicialAdm.java
+++ View/src/adm/TelaInicialAdm.java
@@ -132,7 +132,7 @@ public class TelaInicialAdm extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 465, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 465, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
